738b426c4cc87afc778aec79416204a2b61f7ec7
Louise <louisegreen18@gmail.com>
2021-11-22 20:53:28 +0000

    code developed for dirty data project
    code developed for dirty data project

--- data_cleaning_scripts/dirty_data_candy.R
+++ data_cleaning_scripts/dirty_data_candy.R
@@ -7,7 +7,7 @@ library(readxl)
 library(dplyr)
 library(here)
 
-# Loading  datasets ---------------------------
+# Loading datasets ---------------------------
 
 candy_2015 <- read_xlsx(here::here("raw_data/boing-boing-candy-2015.xlsx"))
 candy_2016 <- read_xlsx(here::here("raw_data/boing-boing-candy-2016.xlsx"))
@@ -87,16 +87,47 @@ clean_2015 <- clean_2015 %>%
 # I am not sure of the extent to which I am keeping these
 
 clean_2015 <- clean_2015 %>%
-  transform(age = as.numeric(age))
-# changing age column type, and at the same time, changing strings to NA 
-# by coercion
-
-clean_2015 <- clean_2015 %>%
   relocate(necco_wafers, .after = york_peppermint_patties)
 # moving the necco wafers column to after york peppermint
 
 clean_2015$year <- 2015
 # reassigning the year column to 2015
+
+# Cleaning the age column
+# There are 12 answers which indicate a specific answer, e.g. "40. Deal with it".
+# There are multiple which reference a vague answer, e.g "50s". It is not
+# reasonable to assume their answer - if there is time, I will go back and
+# replace these with "vague" (or similar). Then there are the outright "invalid"
+# answers, e.g. "old enough" which do not reference an age. These are NA.
+
+clean_2015$age[clean_2015$age == "45, but the 8-year-old Huntress and
+                 bediapered Unicorn give me political cover and social
+                 respectability.  However, I WILL eat more than they do
+                 combined."] <- 45
+clean_2015$age[clean_2015$age == "37 (I'm taking a child)"] <- 37
+clean_2015$age[clean_2015$age == "Good Lord!  I'm 43!"] <- 43
+clean_2015$age[clean_2015$age == "46:"] <- 46
+clean_2015$age[clean_2015$age == "40. Deal with it. "] <- 40
+clean_2015$age[clean_2015$age == "37,"] <- 37
+clean_2015$age[clean_2015$age == "50 (despair)"] <- 50
+clean_2015$age[clean_2015$age == "27^"] <- 27
+clean_2015$age[clean_2015$age == "50, taking a 13 year old."] <- 50
+clean_2015$age[clean_2015$age == "42 - I'm taking my kid"] <- 42
+clean_2015$age[clean_2015$age == "42 - I'm taking my kid"] <- 42
+clean_2015$age[clean_2015$age == "50t"] <- 50 
+
+
+# There are also 12 column names with vague answers. If I have time, I will
+# go back and correct these.
+# For now, I will drop them.
+
+clean_2015 <- clean_2015 %>%
+  transform(age = as.numeric(age))
+# changing age column type, and at the same time, changing strings to NA 
+# by coercion
+
+# Add a gender column??
+
 
 # 2016 CLEANING ---------------------------------------------------------------
 
@@ -112,10 +143,16 @@ clean_2016 <- clean_2016 %>%
          state = which_state_province_county_do_you_live_in,
          hersheys_milk_chocolate = hershey_s_milk_chocolate,
   )
-# cleaning column names
-
+# There are none further to change.
 # the columns at the end, i.e "please estimate" etc have NOT been changed, as 
 # I am not sure of the extent to which I am keeping these
+
+# Cleaning the age column
+# There are none which indicate a specific answer.
+# There are 5 which reference a vague answer, e.g "50s". It is not
+# reasonable to assume their answer - if there is time, I will go back and
+# replace these with "vague" (or similar). Then there are the outright "invalid"
+# answers, e.g. "old enough" which do not reference an age. These are NA.
 
 clean_2016 <- clean_2016 %>%
   transform(age = as.numeric(age))
@@ -123,4 +160,90 @@ clean_2016 <- clean_2016 %>%
 # by coercion
 
 clean_2016$year <- 2016
-# reassigning the year column to 2015
+# reassigning the year column to 2016
+
+# Cleaning the country column
+# Cleaning country names
+
+
+
+# Change all versions of USA to USA, and similar for other countries.
+
+# Remove incorrect strings. 
+
+# Clean state column, i.e. ensure all strings are correct.
+
+# Change all USA and canadian states to same format, e.g. TN, USA
+
+
+# 2017 Cleaning --------------------------------------------------------------
+clean_2017 <- clean_names(candy_2017)
+# cleaning column names using clean_names from janitor package
+
+# need to do an across or something to remove q6 from 7-109.
+
+# Manually change these
+# [2] "q1_going_out"                                                                    
+# [3] "q2_gender"                                                                       
+# [4] "q3_age"                                                                          
+# [5] "q4_country"                                                                      
+# [6] "q5_state_province_county_etc"                                                    
+# [110] "q7_joy_other"                                                                    
+# [111] "q8_despair_other"                                                                
+# [112] "q9_other_comments"                                                               
+# [113] "q10_dress"                                                                       
+# [114] "x114"                                                                            
+# [115] "q11_day"                                                                         
+# [116] "q12_media_daily_dish"                                                            
+# [117] "q12_media_science"                                                               
+# [118] "q12_media_espn"                                                                  
+# [119] "q12_media_yahoo"                                                                 
+# [120] "click_coordinates_x_y"  
+
+# clean_2015 <- clean_2015 %>%
+#   rename(age = how_old_are_you,
+#   )
+# cleaning column names
+
+# Remove column 114
+
+
+clean_2017$internal_id <- 2017
+# reassigning the internal_id column to 2017
+
+# Cleaning the age column
+# There are 2 answers which indicate a specific answer, e.g. "40. Deal with it".
+# There are 5 which reference a vague answer, e.g. "50's". It is not
+# reasonable to assume their answer - if there is time, I will go back and
+# replace these with "vague" (or similar). Then there are the outright "invalid"
+# answers, e.g. "old enough" which do not reference an age. These are NA.
+
+
+# These are the two to be corrected
+# sixty-nine
+# 46 Halloweens.
+
+
+#Syntax
+clean_2015$age[clean_2015$age == "45, but the 8-year-old Huntress and
+                 bediapered Unicorn give me political cover and social
+                 respectability.  However, I WILL eat more than they do
+                 combined."] <- 45
+
+clean_2015 <- clean_2015 %>%
+  transform(age = as.numeric(age))
+# Changing age column type, and at the same time, changing strings to NA 
+# by coercion
+
+# Cleaning country names
+
+
+country names; ensure all countries are capitalised.
+
+# Change all versions of USA to USA, and similar for other countries.
+
+# Remove incorrect strings. 
+
+# Clean state column, i.e. ensure all strings are correct.
+
+# Change all USA and canadian states to same format, e.g. TN, USA
